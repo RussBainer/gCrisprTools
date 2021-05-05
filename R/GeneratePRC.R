f547ccd9b970eb26553fff07b93c4029488aebb0
RussBainer <rbainer@gmail.com>
2021-05-05 15:41:29 -0700

    ctPRC core code update

--- R/GeneratePRC.R
+++ R/GeneratePRC.R
@@ -9,40 +9,49 @@
 ##' somewhat anticonservative in cases where the gene ranks are not well differentiated.  
 ##'
 ##' @param summaryDF A dataframe summarizing the results of the screen, returned by the function \code{\link{ct.generateResults}}. 
-##' @param target.list A character vector containing the names of the targets to be tested. Only targets contained in the \code{geneID} 
-##' column of the provided \code{summaryDF} are considered.
-##' @param stat The statistic to use when ordering the genes. Must be one of \code{"enrich.p"}, \code{"deplete.p"}, \code{"enrich.fc"}, 
-##' or \code{"deplete.fc"}. 
+##' @param target.list A character vector containing the names of the targets to be tested; by default these are assumed to be `geneID`s, 
+##' but specifying `collapse=geneSymbol` enables setting on `geneSymbol` by passing that value through to `ct.simpleResult`.
+##' @param direction Direction by which to order target signals (`enrich` or `deplete`).  
 ##' @param plot.it Logical value indicating whether to plot the curves. 
 ##' @return A list containing the the x and y coordinates of the curve.
 ##' @author Russell Bainer
 ##' @examples data('resultsDF')
 ##' data('essential.genes') #Note that this is an artificial example.
-##' pr <- ct.PRC(resultsDF, essential.genes, 'enrich.p')
+##' pr <- ct.PRC(resultsDF, essential.genes, 'enrich')
 ##' str(pr)
 ##' @export
 
 ct.PRC <-
   function(summaryDF,
            target.list,
-           stat = c("enrich.p", "deplete.p", "enrich.fc", "deplete.fc", "enrich.rho", "deplete.rho"), 
-           plot.it = TRUE) {
+           direction = c("enrich", "deplete"), 
+           plot.it = TRUE, 
+           ...) {
     
   
     #Check the input: 
-    if(!ct.resultCheck(summaryDF)){
-      stop("Execution halted.")
-    }
+    #if(!ct.resultCheck(summaryDF)){
+    #  stop("Execution halted.")
+    #}
 
     #Convert to gene-level stats
-    summaryDF <- summaryDF[!duplicated(summaryDF$geneID),]
-    row.names(summaryDF) <- summaryDF$geneID
+    #summaryDF <- summaryDF[!duplicated(summaryDF$geneID),]
+    #row.names(summaryDF) <- summaryDF$geneID
+    
+    direction <- match.arg(direction)
+    stopifnot(is(plot.it, 'logical'))
+    
+    if(!exists("collapse")){
+      collapse <- 'geneID'
+    }
+    simpleDF <- ct.simpleResult(summaryDF, collapse)
     
     if(!is.character(target.list)){
       warning("Supplied target.list is not a character vector. Coercing.")
       target.list <- as.character(target.list)
     }
-    present <- intersect(target.list, summaryDF$geneID)
+    present <- intersect(target.list, row.names(simpleDF))
+    
     if(length(present) != length(target.list)){
       if(length(present) < 1){
         stop("None of the genes in the input list are present in the geneSymbol column of the input data.frame.")
@@ -50,37 +59,19 @@ ct.PRC <-
       warning(paste(length(present), "of", length(target.list), "genes are present in the supplied results data.frame. Ignoring the remainder of the target.list."))
     }
     
-    #Gather the values for the targets: 
-    stat <- match.arg(stat)
-    targvals <- switch(stat, 
-         enrich.p = (summaryDF[(summaryDF$geneID %in% present),"Target-level Enrichment P"]), 
-         deplete.p = (summaryDF[(summaryDF$geneID %in% present),"Target-level Depletion P"]), 
-         enrich.fc = (-summaryDF[(summaryDF$geneID %in% present),"Median log2 Fold Change"]), 
-         deplete.fc = (summaryDF[(summaryDF$geneID %in% present),"Median log2 Fold Change"]),
-         enrich.rho = (summaryDF[(summaryDF$geneID %in% present),"Rho_enrich"]),
-         deplete.rho = (summaryDF[(summaryDF$geneID %in% present),"Rho_deplete"])
-    )   
-    #Extract the appropriate stat. 
-    values <- switch(stat, 
-        enrich.p = sort(summaryDF[,"Target-level Enrichment P"]), 
-        deplete.p = sort(summaryDF[,"Target-level Depletion P"]), 
-        enrich.fc = sort(-summaryDF[,"Median log2 Fold Change"]), 
-        deplete.fc = sort(summaryDF[,"Median log2 Fold Change"]),
-        enrich.rho = sort(summaryDF[,"Rho_enrich"]), 
-        deplete.rho = sort(summaryDF[,"Rho_deplete"])
-    )
+    #Subset signals
+    values <- simpleDF[simpleDF$direction == direction,]
+    values <- c(values$best.p[order(values$best.p, decreasing =FALSE)],1)
 
+    targvals <- vapply(target.list, function(x){ifelse(simpleDF[x,'direction'] %in% direction, values[x, 'best.p'], 1)}, numeric(1))
+    
     out <- list()
     out$precision <- c(1, unlist(lapply(unique(values), function(x){sum(targvals <= x, na.rm = TRUE)/sum(values <= x, na.rm= TRUE)})), 0)
     out$recall <- c(0, unlist(lapply(unique(values), function(x){sum(targvals <= x, na.rm = TRUE)/length(targvals)})), 1)
     
-    enrich <- switch(stat, 
-                     enrich.p = ct.targetSetEnrichment(summaryDF, target.list, enrich = TRUE),
-                     deplete.p =  ct.targetSetEnrichment(summaryDF, target.list, enrich = FALSE),
-                     enrich.fc =  ct.targetSetEnrichment(summaryDF, target.list, enrich = TRUE),
-                     deplete.fc =  ct.targetSetEnrichment(summaryDF, target.list, enrich = FALSE),
-                     enrich.rho = ct.targetSetEnrichment(summaryDF, target.list, enrich = TRUE),
-                     deplete.rho = ct.targetSetEnrichment(summaryDF, target.list, enrich = FALSE)
+    enrich <- switch(direction, 
+                     enrich = ct.targetSetEnrichment(simpleDF, target.list, enrich = TRUE),
+                     deplete =  ct.targetSetEnrichment(simpleDF, target.list, enrich = FALSE)
     )
     out <- c(out, enrich)
     
